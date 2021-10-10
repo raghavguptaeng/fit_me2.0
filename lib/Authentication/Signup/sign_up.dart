3012c81b04d43112fe1ed509d54c2caa9d7c4ab5
Raghav Gupta <E19CSE258@bennett.edu.in>
2021-10-10 13:30:40 +0530

    Signup Screen UI Done

--- lib/Authentication/Signup/sign_up.dart
+++ lib/Authentication/Signup/sign_up.dart
@@ -10,99 +10,135 @@ class SignUp extends StatelessWidget {
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TopHeading(),
-            SizedBox(
-              height: height * 0.4,
-              width: width * 0.8,
-              child: ListView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: kInputTextDecoration.copyWith(
-                          hintText: "First Name",
-                        prefixIcon: Icon(Icons.person),
-                      ),
-                      onChanged: (value){
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: height*0.95,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TopHeading(),
+                SizedBox(
+                  height: height * 0.4,
+                  width: width * 0.8,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: kInputTextDecoration.copyWith(
+                              hintText: "First Name",
+                            prefixIcon: Icon(Icons.person),
+                          ),
+                          onChanged: (value){
 
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(
-                        fontFamily: "Poppins",
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: kInputTextDecoration.copyWith(
-                        prefixIcon: Icon(Icons.person),
-                        labelText: "Last Name",
-                      ),
-                      onChanged: (value){
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: kInputTextDecoration.copyWith(
+                            prefixIcon: Icon(Icons.person),
+                            labelText: "Last Name",
+                          ),
+                          onChanged: (value){
 
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(
-                        fontFamily: "Poppins",
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: kInputTextDecoration.copyWith(
-                          hintText: "Email",
-                          prefixIcon: Icon(Icons.email_outlined,)),
-                      onChanged: (value){
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: kInputTextDecoration.copyWith(
+                              hintText: "Email",
+                              prefixIcon: Icon(Icons.email_outlined,)),
+                          onChanged: (value){
 
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(
-                        fontFamily: "Poppins",
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: kInputTextDecoration.copyWith(prefixIcon: Icon(Icons.password),
-                        labelText: "Password",),
-                      onChanged: (value){
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: kInputTextDecoration.copyWith(prefixIcon: Icon(Icons.password),
+                            labelText: "Password",),
+                          onChanged: (value){
 
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(
-                        fontFamily: "Poppins",
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: height*0.08,
+                        width: width*0.8,
+                        decoration: kRoundedBigButton,
+                        child: Center(
+                          child: Text("Register",style: kBigTextHeading.copyWith(color: Colors.white),),
+                        ),
+                      ),
+                      RowWithOrandLine(width),
+                      Container(
+                        height: height*0.1,
+                        width: height*0.1,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          border: Border.all(color: Colors.black,width: 2)
+                        ),
+                        child: Center(
+                          child: Image.asset("Assets/Logo/google.png",height: height*0.8,),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    height: height*0.08,
-                    width: width*0.8,
-                    decoration: kRoundedBigButton,
-                    child: Center(
-                      child: Text("Register",style: kBigTextHeading.copyWith(color: Colors.white),),
-                    ),
-                  ),
-
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  Row RowWithOrandLine(double width) {
+    return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: width*0.4,
+                      height: 0.5,
+                      color: Colors.black,
+                    ),
+                    Text("OR",style: kBigTextHeading.copyWith(fontSize: 20),),
+                    Container(
+                      width: width*0.4,
+                      height: 0.5,
+                      color: Colors.black,
+                    )
+                  ],
+                );
   }
 
   Container TopHeading() {
